5c927e1586874154d92c77a3ccd18474c5cbd91a
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-10 00:50:09 +0200

    feat: Add member middleware to reservation and boat routes for enhanced access control

--- wsco-api/src/routes/authRoutes.ts
+++ wsco-api/src/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import { AuthenticateUser, FinishAuth, GetUserSession, LogoutUser } from '@/controllers/User';
+import { memberMiddleware } from '@/middleware/Member';
 import Router from 'express';
 
 const router = Router();
 
 router.post('/', AuthenticateUser);
 router.post('/finish', FinishAuth);
-router.get('/session', GetUserSession);
-router.get('/logout', LogoutUser);
+router.get('/session', memberMiddleware, GetUserSession);
+router.get('/logout', memberMiddleware, LogoutUser);
 
 export default router;
--- wsco-api/src/routes/boatRoutes.ts
+++ wsco-api/src/routes/boatRoutes.ts
@@ -1,11 +1,13 @@
 import { getAllBoats, getBoatById, createBoat, isBoatOccupied } from "@/controllers/Boat";
+import { BootManagerMiddleware } from "@/middleware/Bootmanager";
+import { memberMiddleware } from "@/middleware/Member";
 import Router from "express";
 
 const router = Router();
 
-router.get("/", getAllBoats);
-router.post("/", createBoat);
-router.get('/isOccupied', isBoatOccupied);
-router.get('/:id', getBoatById);
+router.get("/", memberMiddleware, getAllBoats);
+router.post("/", BootManagerMiddleware, createBoat);
+router.get('/isOccupied', memberMiddleware, isBoatOccupied);
+router.get('/:id', memberMiddleware, getBoatById);
 
 export default router;   
